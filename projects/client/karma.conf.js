532d59a1ed5aa9c07b0082f3c5b0804eebc0754c
Andrew Wiens <adwiens@gmail.com>
2019-09-16 11:22:21 -0500

    Convert SSE client & server projects to use Bazel
    Convert to bazel: "ng add @angular/bazel"
    Create Bazel files: "ng build --leaveBazelFilesOnDisk"

--- projects/client/karma.conf.js
+++ projects/client/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/cliApp802'),
+      dir: require('path').join(__dirname, '../../coverage/deleteme'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
